e38c0455137353f42dd34224d1fbfb1d8de4d3b4
Brian Heim <bheim67@gmail.com>
2016-10-19 10:00:11 -0600

    put logic into controller, working with pry to see if my faraday call is workgin correctly

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,19 @@
 class SearchController < ApplicationController
   def index
-    
+    @nearest_stations = SearchController.stations({location: "80203", radius: 6.0, limit: 10, fuel_type: "ELEC,LPG"})
+    binding.pry
+  end
+
+  def self.conn
+    Farady.new(:url => 'https://developer.nrel.gov/api/alt-fuel-stations/v1/') do |f|
+      f.adapter Faraday.default_adapter
+      f.params[:api_key] = ENV['api_key']
+      f.params[:format] = "json"
+    end
+  end
+
+  def self.stations(filter)
+    response = conn.get '/nearest', filter
+    JSON.parse(response.body, symbolize_names: true)[:results]
   end
 end
