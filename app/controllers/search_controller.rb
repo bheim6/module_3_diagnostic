481da4cfbc5163fb04d7ec86b4feb0d035807897
Brian Heim <bheim67@gmail.com>
2016-10-19 10:41:37 -0600

    got request to work in rails c, was simply removing strings from zip code, even though it worked on postman

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
   end
 
   def conn
-    Faraday.new(:url => "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location='80203'&radius=6.0&limit=10&fuel_type=ELEC,LPG&api_key=35363WNH5zjZuW1znKhT6vFU3Zwr6ShzFi9ZaiR7&format=JSON") do |f|
+    Faraday.new(:url => "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&radius=6.0&limit=10&fuel_type=ELEC,LPG&api_key=35363WNH5zjZuW1znKhT6vFU3Zwr6ShzFi9ZaiR7&format=JSON") do |f|
       f.adapter Faraday.default_adapter
       # f.params[:api_key] = ENV['api_key']
       # f.params[:format] = "JSON"
@@ -17,7 +17,7 @@ class SearchController < ApplicationController
   end
 
   def stations
-    response = conn
+    response = conn.get
     JSON.parse(response.body, symbolize_names: true)[:results]
   end
 end
