a3f68d61bc5f68a685de9199d312dedddb02955e
Brian Heim <bheim67@gmail.com>
2016-10-19 10:31:44 -0600

    change faraday to have entire url, still trying to get a good response back

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,23 +1,23 @@
 class SearchController < ApplicationController
   def index
-    @nearest_stations = SearchController.stations
+    @nearest_stations = stations
     binding.pry
   end
 
-  def self.conn
-    Faraday.new(:url => 'https://developer.nrel.gov') do |f|
+  def conn
+    Faraday.new(:url => "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location='80203'&radius=6.0&limit=10&fuel_type=ELEC,LPG&api_key=35363WNH5zjZuW1znKhT6vFU3Zwr6ShzFi9ZaiR7&format=JSON") do |f|
       f.adapter Faraday.default_adapter
-      f.params[:api_key] = ENV['api_key']
-      f.params[:format] = "json"
-      f.params[:loaction] = "80203"
-      f.params[:radius] = 6.0
-      f.params[:limit] = 10
-      f.params[:fuel_type] = "ELEC,LPG"
+      # f.params[:api_key] = ENV['api_key']
+      # f.params[:format] = "JSON"
+      # f.params[:location] = "80203"
+      # f.params[:radius] = 6.0
+      # f.params[:limit] = 10
+      # f.params[:fuel_type] = "ELEC,LPG"
     end
   end
 
-  def self.stations
-    response = conn.get '/api/alt-fuel-stations/v1/nearest'
+  def stations
+    response = conn
     JSON.parse(response.body, symbolize_names: true)[:results]
   end
 end
